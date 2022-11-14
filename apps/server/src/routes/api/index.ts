d5c8dedae3c8b9e6aca8d7ba99f45f4d7d3dab5f
Pariring <paring@pikokr.dev>
2022-11-14 21:30:25 +0900

    feat: kimubabo

--- apps/server/src/routes/api/index.ts
+++ apps/server/src/routes/api/index.ts
@@ -2,7 +2,9 @@ import { FastifyPluginAsync } from 'fastify'
 import { nodesRoutes } from './nodes.js'
 
 export const apiRoutes: FastifyPluginAsync = async (server) => {
-  server.get('/', () => ({ hello: 'world' }))
+  server.get<{ Querystring: { name?: string } }>('/', (req) => ({
+    message: `${req.query.name ?? 'kimu'} is babo`,
+  }))
 
   await server.register(nodesRoutes)
 }
